21e72a6dac3aa3e24d1c2e27e7af67e84fce5401
Ritik Porwal <109209220+ritikporwal710@users.noreply.github.com>
2025-04-30 16:37:04 +0530

    Update WebConfig.java

--- src/main/java/com/financetrack/development/config/WebConfig.java
+++ src/main/java/com/financetrack/development/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // your frontend port
+                .allowedOrigins("https://trafin-client.onrender.com") // your frontend port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie", "Clear-Site-Data") // Add this line
